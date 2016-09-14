44c4acfa6a1df6ab7325f014e3210d0c054f7b4b
Maryna Yanul <maryna.yanul@gmail.com>
2016-09-14 19:06:24 +0300

    Unicorn battle (done)

--- js/main.js
+++ js/main.js
@@ -6,3 +6,11 @@ function battleCavalier() {
   document.getElementById("fire").className = "fire fire--animate";
   setTimeout(function d () { document.location.href='dead.html'} , 6000);
 }
+
+function battleUnicorn() {
+  document.getElementById("dragon-battle").className = "dragon-battle dragon-battle--animate";
+  document.getElementById("fire").className = "fire fire--animate";
+  document.getElementById("unicorn-hero").className = "unicorn-hero unicorn-hero--animate";
+  document.getElementById("rainbow").className = "rainbow--animate";
+  setTimeout(function d () { document.location.href='dead.html'} , 6000);
+}
